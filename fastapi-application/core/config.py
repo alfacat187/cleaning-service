52c87479a5812291c2a3cedcc12ab895e0820c68
alexander <alex.matveev.dev@gmail.com>
2025-02-27 20:16:59 +0300

    update config

--- fastapi-application/core/config.py
+++ fastapi-application/core/config.py
@@ -7,9 +7,9 @@ from typing import Literal
 
 from pydantic import (
     BaseModel,
+    EmailStr,
     PostgresDsn,
     computed_field,
-    EmailStr,
 )
 from pydantic_settings import (
     BaseSettings,
@@ -17,9 +17,7 @@ from pydantic_settings import (
 )
 
 BASE_DIR = Path(__file__).parent.parent
-LOG_DEFAULT_FORMAT = (
-    "[%(asctime)s.%(msecs)03d] %(module)10s:%(lineno)-3d %(levelname)-7s - %(message)s"
-)
+LOG_DEFAULT_FORMAT = "[%(asctime)s.%(msecs)03d] %(module)10s:%(lineno)-3d %(levelname)-7s - %(message)s"
 DATE_FORMAT = "%Y-%m-%dT%H:%M:%S%z"
 
 
@@ -131,7 +129,7 @@ class PaginationConfig(BaseModel):
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
-        env_file=(".env.template", ".env"),
+        env_file=BASE_DIR.parent / ".env",
         case_sensitive=False,
         env_nested_delimiter="__",
         env_prefix="APP_CONFIG__",
